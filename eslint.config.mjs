2fa7bf47da6d66df8f7e9f918842e599741e55bf
777Makss777 <777docmax777@gmail.com>
2026-05-03 12:40:14 +0200

    ....

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import js from '@eslint/js';
+
+export default [
+  {
+    ignores: ['node_modules'],
+  },
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+];
