1fa16cd594272dfc5f326439ef4ab20cd42b4614
unknown <777docmax777@gmail.com>
2026-05-26 10:32:06 +0200

    ....

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -63,6 +63,8 @@ export const refreshUserSession = async (req, res) => {
   }
 
   if (session.refreshTokenValidUntil < new Date()) {
+    await Session.deleteOne({ _id: session._id });
+    clearSessionCookies(res);
     throw createHttpError(401, 'Session token expired');
   }
 
--- src/models/note.js
+++ src/models/note.js
@@ -31,7 +31,7 @@ const noteSchema = new Schema(
   },
 );
 
-noteSchema.index({ tag: 1 });
+noteSchema.index({ tag: 1, userId: 1 });
 noteSchema.index({ title: 'text', content: 'text' });
 
 export const Note = model('Note', noteSchema);
